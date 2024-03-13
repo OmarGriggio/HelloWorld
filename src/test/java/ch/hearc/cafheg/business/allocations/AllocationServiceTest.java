200d4402de41924654588dc1e550d1af146267f0
Omar <omar.griggio@he-arc.ch>
2024-03-13 15:24:49 +0100

    first commit

---
+++ src/test/java/ch/hearc/cafheg/business/allocations/AllocationServiceTest.java
@@ -0,0 +1,103 @@
+package ch.hearc.cafheg.business.allocations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import ch.hearc.cafheg.business.common.Montant;
+import ch.hearc.cafheg.infrastructure.persistance.AllocataireMapper;
+import ch.hearc.cafheg.infrastructure.persistance.AllocationMapper;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class AllocationServiceTest {
+
+  private AllocationService allocationService;
+
+  private AllocataireMapper allocataireMapper;
+  private AllocationMapper allocationMapper;
+
+
+  @BeforeEach
+  void setUp() {
+    allocataireMapper = Mockito.mock(AllocataireMapper.class);
+    allocationMapper = Mockito.mock(AllocationMapper.class);
+
+    allocationService = new AllocationService(allocataireMapper, allocationMapper);
+  }
+
+  @Test
+  void findAllAllocataires_GivenEmptyAllocataires_ShouldBeEmpty() {
+    Mockito.when(allocataireMapper.findAll("Geiser")).thenReturn(Collections.emptyList());
+    List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
+    assertThat(all).isEmpty();
+  }
+
+  @Test
+  void findAllAllocataires_Given2Geiser_ShouldBe2() {
+    Mockito.when(allocataireMapper.findAll("Geiser"))
+        .thenReturn(Arrays.asList(new Allocataire(new NoAVS("1000-2000"), "Geiser", "Arnaud"),
+            new Allocataire(new NoAVS("1000-2001"), "Geiser", "Aurélie")));
+    List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
+    assertAll(() -> assertThat(all.size()).isEqualTo(2),
+        () -> assertThat(all.get(0).getNoAVS()).isEqualTo(new NoAVS("1000-2000")),
+        () -> assertThat(all.get(0).getNom()).isEqualTo("Geiser"),
+        () -> assertThat(all.get(0).getPrenom()).isEqualTo("Arnaud"),
+        () -> assertThat(all.get(1).getNoAVS()).isEqualTo(new NoAVS("1000-2001")),
+        () -> assertThat(all.get(1).getNom()).isEqualTo("Geiser"),
+        () -> assertThat(all.get(1).getPrenom()).isEqualTo("Aurélie"));
+  }
+
+  @Test
+  void findAllocationsActuelles() {
+    Mockito.when(allocationMapper.findAll())
+        .thenReturn(Arrays.asList(new Allocation(new Montant(new BigDecimal(1000)), Canton.NE,
+            LocalDate.now(), null), new Allocation(new Montant(new BigDecimal(2000)), Canton.FR,
+            LocalDate.now(), null)));
+    List<Allocation> all = allocationService.findAllocationsActuelles();
+    assertAll(() -> assertThat(all.size()).isEqualTo(2),
+        () -> assertThat(all.get(0).getMontant()).isEqualTo(new Montant(new BigDecimal(1000))),
+        () -> assertThat(all.get(0).getCanton()).isEqualTo(Canton.NE),
+        () -> assertThat(all.get(0).getDebut()).isEqualTo(LocalDate.now()),
+        () -> assertThat(all.get(0).getFin()).isNull(),
+        () -> assertThat(all.get(1).getMontant()).isEqualTo(new Montant(new BigDecimal(2000))),
+        () -> assertThat(all.get(1).getCanton()).isEqualTo(Canton.FR),
+        () -> assertThat(all.get(1).getDebut()).isEqualTo(LocalDate.now()),
+        () -> assertThat(all.get(1).getFin()).isNull());
+  }
+
+  @Test
+  void getParentDroitAllocation_GivenResidence_ShoulBeParentSameResidence(){
+    HashMap<String, Object> map = new HashMap<>();
+    map.put("enfantResidence","Neuchâtel");
+    map.put("parent1Residence","Neuchâtel");
+    map.put("parent2Residence","Bienne");
+    map.put("parent1ActiviteLucrative",true);
+    map.put("parent2ActiviteLucrative",true);
+    map.put("parent1Salaire",2500);
+    map.put("parent2Salaire",3000);
+
+    Assertions.assertEquals("Parent2",allocationService.getParentDroitAllocation(map));
+  }
+}
+  /*
+  // Headers de la requête HTTP doit contenir "Content-Type: application/json"
+  // BODY de la requête HTTP à transmettre afin de tester le endpoint
+  {
+      "enfantResidence" : "Neuchâtel",
+      "parent1Residence" : "Neuchâtel",
+      "parent2Residence" : "Bienne",
+      "parent1ActiviteLucrative" : true,
+      "parent2ActiviteLucrative" : true,
+      "parent1Salaire" : 2500,
+      "parent2Salaire" : 3000
+  }
+   */
